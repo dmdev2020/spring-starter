b194b62da14eb3ef25619ae791fed48c69f059c9
dmatveyenka <dmdev2020@gmail.com>
2021-11-14 11:49:04 +0100

    lesson - 5

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,23 +1,14 @@
 package com.dmdev.spring;
 
-import com.dmdev.spring.ioc.Container;
-import com.dmdev.spring.service.UserService;
+import com.dmdev.spring.database.pool.ConnectionPool;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var container = new Container();
-
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        var userService = container.get(UserService.class);
-        // TODO: 14.11.2021 userService 
+        var context = new ClassPathXmlApplicationContext("application.xml");
+//      clazz -> String -> Map<String, Object>
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
