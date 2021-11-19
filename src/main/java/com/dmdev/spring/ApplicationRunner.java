709fb17fccb233f030428cf729e0c3fe4c66d3dc
dmatveyenka <dmdev2020@gmail.com>
2021-11-19 10:42:50 +0100

    lesson - 10

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -7,12 +7,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
-//      clazz -> String -> Map<String, Object>
-        var connectionPool = context.getBean("p1", ConnectionPool.class);
-        System.out.println(connectionPool);
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+    //      clazz -> String -> Map<String, Object>
+            var connectionPool = context.getBean("p1", ConnectionPool.class);
+            System.out.println(connectionPool);
 
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(companyRepository);
+        }
     }
 }
--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -1,9 +1,11 @@
 package com.dmdev.spring.database.pool;
 
+import org.springframework.beans.factory.InitializingBean;
+
 import java.util.List;
 import java.util.Map;
 
-public class ConnectionPool {
+public class ConnectionPool implements InitializingBean {
 
     private final String username;
     private final Integer poolSize;
@@ -22,5 +24,18 @@ public class ConnectionPool {
 
     public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
+    }
+
+    private void init() {
+        System.out.println("Init connection pool");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Properties set");
+    }
+
+    private void destroy() {
+        System.out.println("Clean connection pool");
     }
 }
