86878bb771e32445f025a43655497d168b69b0a5
dmatveyenka <dmdev2020@gmail.com>
2021-11-19 09:38:44 +0100

    lesson - 6

--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -1,4 +1,22 @@
 package com.dmdev.spring.database.pool;
 
+import java.util.List;
+import java.util.Map;
+
 public class ConnectionPool {
+
+    private final String username;
+    private final Integer poolSize;
+    private final List<Object> args;
+    private final Map<String, Object> properties;
+
+    public ConnectionPool(String username,
+                          Integer poolSize,
+                          List<Object> args,
+                          Map<String, Object> properties) {
+        this.username = username;
+        this.poolSize = poolSize;
+        this.args = args;
+        this.properties = properties;
+    }
 }
