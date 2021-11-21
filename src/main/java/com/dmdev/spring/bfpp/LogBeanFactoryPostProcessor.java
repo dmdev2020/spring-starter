dae4bd1730f6420c6a648a56b3a1ef70d0bc93a5
dmatveyenka <dmdev2020@gmail.com>
2021-11-21 17:39:59 +0100

    lesson - 13

---
+++ src/main/java/com/dmdev/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,27 @@
+package com.dmdev.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.ConstructorArgumentValues;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        for (String beanDefinitionName : beanFactory.getBeanDefinitionNames()) {
+            var beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
+            var genericArgumentValues = beanDefinition.getConstructorArgumentValues().getGenericArgumentValues();
+            for (ConstructorArgumentValues.ValueHolder genericArgumentValue : genericArgumentValues) {
+                // TODO: 20.11.2021 set get
+            }
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
---
+++ src/main/java/com/dmdev/spring/bfpp/VerifyPropertyBeanFactoryPostProcessor.java
@@ -0,0 +1,19 @@
+package com.dmdev.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+
+public class VerifyPropertyBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
