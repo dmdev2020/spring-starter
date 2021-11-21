7290a3206052fa6ece5c6880e0cb50535cfeae95
dmatveyenka <dmdev2020@gmail.com>
2021-11-21 13:04:57 +0100

    lesson - 12

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -2,11 +2,19 @@ package com.dmdev.spring;
 
 import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CompanyRepository;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
     //      clazz -> String -> Map<String, Object>
             var connectionPool = context.getBean("p1", ConnectionPool.class);
