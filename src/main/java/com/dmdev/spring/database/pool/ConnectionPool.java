2b13ce0f8434fe53143fa533c8a90a16740d66aa
dmatveyenka <dmdev2020@gmail.com>
2021-11-27 14:18:38 +0100

    lesson - 14

--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -2,6 +2,8 @@ package com.dmdev.spring.database.pool;
 
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Map;
 
@@ -26,6 +28,7 @@ public class ConnectionPool implements InitializingBean {
         this.properties = properties;
     }
 
+    @PostConstruct
     private void init() {
         System.out.println("Init connection pool");
     }
@@ -35,6 +38,7 @@ public class ConnectionPool implements InitializingBean {
         System.out.println("Properties set");
     }
 
+    @PreDestroy
     private void destroy() {
         System.out.println("Clean connection pool");
     }
