65c08cedd39a3995f85906b3f34d63b01fd8fd7b
dmatveyenka <dmdev2020@gmail.com>
2021-11-27 14:55:42 +0100

    lesson - 16

---
+++ src/main/java/com/dmdev/spring/bpp/InjectBeanPostProcessor.java
@@ -0,0 +1,32 @@
+package com.dmdev.spring.bpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.util.ReflectionUtils;
+
+import java.util.Arrays;
+
+public class InjectBeanPostProcessor implements BeanPostProcessor, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(InjectBean.class))
+                .forEach(field -> {
+                    Object beanToInject = applicationContext.getBean(field.getType());
+                    ReflectionUtils.makeAccessible(field);
+                    ReflectionUtils.setField(field, bean, beanToInject);
+                });
+
+        return bean;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -1,16 +1,10 @@
 package com.dmdev.spring.database.repository;
 
+import com.dmdev.spring.bpp.InjectBean;
 import com.dmdev.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
-
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
