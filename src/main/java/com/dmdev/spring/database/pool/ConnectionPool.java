c255b7ab4a7e5b1bcae6176a6e4793e9c035fc02
dmatveyenka <dmdev2020@gmail.com>
2021-11-28 09:55:18 +0100

    lesson - 18

--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -9,10 +9,13 @@ import java.util.Map;
 
 public class ConnectionPool implements InitializingBean {
 
-    private final String username;
-    private final Integer poolSize;
-    private final List<Object> args;
+    private String username;
+    private Integer poolSize;
+    private List<Object> args;
     private Map<String, Object> properties;
+
+    public ConnectionPool() {
+    }
 
     public ConnectionPool(String username,
                           Integer poolSize,
--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -1,20 +1,27 @@
 package com.dmdev.spring.database.repository;
 
 import com.dmdev.spring.bpp.Auditing;
-import com.dmdev.spring.bpp.InjectBean;
 import com.dmdev.spring.bpp.Transaction;
 import com.dmdev.spring.database.entity.Company;
 import com.dmdev.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+//    @Resource(name = "pool1")
+//    @Qualifier("pool1")
+    private ConnectionPool pool1;
+    @Autowired
+    private List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
     @PostConstruct
     private void init() {
@@ -30,5 +37,10 @@ public class CompanyRepository implements CrudRepository<Integer, Company> {
     @Override
     public void delete(Company entity) {
         System.out.println("delete method...");
+    }
+
+    @Autowired
+    public void setPool1(ConnectionPool pool1) {
+        this.pool1 = pool1;
     }
 }
