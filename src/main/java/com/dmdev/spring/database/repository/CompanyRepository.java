5756ba4042fb2e6679cb940e6ad560fb7f99cfe8
dmatveyenka <dmdev2020@gmail.com>
2021-12-04 12:15:10 +0100

    lesson - 21

--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -11,7 +11,6 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
