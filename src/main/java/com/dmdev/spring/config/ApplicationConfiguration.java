df01fcd3efb51d9267293f21499e7b0b15237a60
dmatveyenka <dmdev2020@gmail.com>
2021-12-05 11:06:32 +0100

    lesson - 26

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,19 @@
 package com.dmdev.spring.config;
 
+import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //@ImportResource("classpath:application.xml")
@@ -22,4 +28,28 @@ import org.springframework.stereotype.Component;
                 @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
         })
 public class ApplicationConfiguration {
+
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/dmdev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.dmdev.spring.database.repository;
 
 import com.dmdev.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ public class UserRepository {
 
     private final ConnectionPool connectionPool;
 
-    public UserRepository(ConnectionPool connectionPool) {
+    public UserRepository(@Qualifier("pool2") ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
     }
 }
