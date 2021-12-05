6870e2d75f2e84626126e2c87a57fe0bda40dcf4
dmatveyenka <dmdev2020@gmail.com>
2021-12-05 10:40:46 +0100

    lesson - 25

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,17 @@
 package com.dmdev.spring.config;
 
 import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.web.config.WebConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.dmdev.spring",
---
+++ src/main/java/com/dmdev/web/config/WebConfiguration.java
@@ -0,0 +1,7 @@
+package com.dmdev.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+}
