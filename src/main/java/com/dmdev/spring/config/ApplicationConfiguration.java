b10530a1ee578c69df18b957572fb6b8231581c6
dmatveyenka <dmdev2020@gmail.com>
2021-12-05 14:55:11 +0100

    lesson - 27

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 
 //@ImportResource("classpath:application.xml")
 @Import(WebConfiguration.class)
-@Configuration
+@Configuration(proxyBeanMethods = true)
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.dmdev.spring",
         useDefaultFilters = false,
@@ -36,8 +36,21 @@ public class ApplicationConfiguration {
     }
 
     @Bean
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 25);
+    }
+
+    @Bean
     public UserRepository userRepository2(ConnectionPool pool2) {
         return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository3() {
+        var connectionPool1 = pool3();
+        var connectionPool2 = pool3();
+        var connectionPool3 = pool3();
+        return new UserRepository(pool3());
     }
 }
 
