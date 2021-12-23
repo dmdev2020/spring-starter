1c9504001ead77bef0a42a98721727433c5fbcb7
dmatveyenka <dmdev2020@gmail.com>
2021-12-23 09:09:54 +0100

    lesson - 33

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,32 +1,12 @@
 package com.dmdev.spring;
 
-import com.dmdev.spring.config.ApplicationConfiguration;
-import com.dmdev.spring.database.pool.ConnectionPool;
-import com.dmdev.spring.service.CompanyService;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.Serializable;
-
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        String value = "hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
-
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web", "prod");
-            context.refresh();
-
-    //      clazz -> String -> Map<String, Object>
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
