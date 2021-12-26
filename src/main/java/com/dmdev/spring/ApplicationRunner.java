59534e1a7fca74260aa152e2cb7b7e39fcc218ef
dmatveyenka <dmdev2020@gmail.com>
2021-12-26 20:50:05 +0100

    lesson - 36

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -9,5 +9,6 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class, args);
         System.out.println(context.getBeanDefinitionCount());
+        System.out.println(context.getBean("pool1"));
     }
 }
