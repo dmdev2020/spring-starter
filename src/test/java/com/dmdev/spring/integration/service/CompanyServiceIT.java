c0dfb34fedb0da4ccb5722f7c5b36e5f3f45b290
dmatveyenka <dmdev2020@gmail.com>
2022-01-04 09:10:59 +0100

    lesson - 43

---
+++ src/test/java/com/dmdev/spring/integration/service/CompanyServiceIT.java
@@ -0,0 +1,31 @@
+package com.dmdev.spring.integration.service;
+
+import com.dmdev.spring.dto.CompanyReadDto;
+import com.dmdev.spring.service.CompanyService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
+public class CompanyServiceIT {
+
+    private static final Integer COMPANY_ID = 1;
+
+    @Autowired
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        var actualResult = companyService.findById(COMPANY_ID);
+
+        assertTrue(actualResult.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
+    }
+}
