d28478755006a409a9ccad7af290b0d5de10f4bd
dmatveyenka <dmdev2020@gmail.com>
2022-01-04 09:27:49 +0100

    lesson - 44

---
+++ src/test/java/com/dmdev/spring/integration/annotation/IT.java
@@ -0,0 +1,16 @@
+package com.dmdev.spring.integration.annotation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@SpringBootTest
+public @interface IT {
+}
--- src/test/java/com/dmdev/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/dmdev/spring/integration/service/CompanyServiceIT.java
@@ -1,23 +1,25 @@
 package com.dmdev.spring.integration.service;
 
+import com.dmdev.spring.config.DatabaseProperties;
 import com.dmdev.spring.dto.CompanyReadDto;
+import com.dmdev.spring.integration.annotation.IT;
 import com.dmdev.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
