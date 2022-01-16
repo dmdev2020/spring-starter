68260818444d8713a3b74ac2840fdb1bddea7353
dmatveyenka <dmdev2020@gmail.com>
2022-01-16 10:39:57 +0100

    lesson - 49

---
+++ src/test/java/com/dmdev/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.dmdev.spring.integration.database.repository;
+
+import com.dmdev.spring.database.entity.Company;
+import com.dmdev.spring.integration.annotation.IT;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@IT
+@RequiredArgsConstructor
+@Transactional
+//@Commit
+class CompanyRepositoryTest {
+
+    private final EntityManager entityManager;
+
+    @Test
+    void findById() {
+        var company = entityManager.find(Company.class, 1);
+        assertNotNull(company);
+        assertThat(company.getLocales()).hasSize(2);
+    }
+
+    @Test
+    void save() {
+        var company = Company.builder()
+            .name("Apple1")
+            .locales(Map.of(
+                "ru", "Apple описание",
+                "en", "Apple description"
+            ))
+            .build();
+        entityManager.persist(company);
+        assertNotNull(company.getId());
+    }
+}
