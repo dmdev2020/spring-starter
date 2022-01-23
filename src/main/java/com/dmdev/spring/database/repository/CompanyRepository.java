0fc224338fadad6c050d3c468d16fd8a052a1808
dmatveyenka <dmdev2020@gmail.com>
2022-01-23 11:11:02 +0100

    lesson - 54

--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -1,11 +1,11 @@
 package com.dmdev.spring.database.repository;
 
 import com.dmdev.spring.database.entity.Company;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface CompanyRepository extends Repository<Company, Integer> {
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
 
     Optional<Company> findById(Integer id);
 
