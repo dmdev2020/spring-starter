5d00701ebc4583f7de6d3fba847ce56f0bfc7238
dmatveyenka <dmdev2020@gmail.com>
2022-01-25 09:18:23 +0100

    lesson - 55

--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -1,13 +1,16 @@
 package com.dmdev.spring.database.repository;
 
 import com.dmdev.spring.database.entity.Company;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface CompanyRepository extends CrudRepository<Company, Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
-    Optional<Company> findById(Integer id);
+    // Optional, Entity, Future
+    Optional<Company> findByName(String name);
 
-    void delete(Company entity);
+    // Collection, Stream (batch, close)
+    List<Company> findAllByNameContainingIgnoreCase(String fragment);
 }
--- src/test/java/com/dmdev/spring/integration/database/repository/CompanyRepositoryTest.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -24,6 +24,12 @@ class CompanyRepositoryTest {
     private final CompanyRepository companyRepository;
 
     @Test
+    void checkFindByQueries() {
+        companyRepository.findByName("Google");
+        companyRepository.findAllByNameContainingIgnoreCase("a");
+    }
+
+    @Test
     void delete() {
         var maybeCompany = companyRepository.findById(APPLE_ID);
         assertTrue(maybeCompany.isPresent());
