15a1b6bb3c358567688ebfe9c4f0420555505c6f
dmatveyenka <dmdev2020@gmail.com>
2022-02-13 17:07:49 +0100

    lesson - 70

--- src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
 
@@ -19,6 +20,9 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @IT
+@Sql({
+    "classpath:sql/data.sql"
+})
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
