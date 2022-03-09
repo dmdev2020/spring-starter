081d04407020b7431399c5ad86f8436929effc06
dmatveyenka <dmdev2020@gmail.com>
2022-03-09 09:31:06 +0100

    lesson 77

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -2,19 +2,24 @@ package com.dmdev.spring.http.controller;
 
 import com.dmdev.spring.database.repository.CompanyRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/api/v1")
 public class GreetingController {
 
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository) {
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
     }
 
+    @GetMapping("/bye")
     public ModelAndView bye() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("greeting/bye");
