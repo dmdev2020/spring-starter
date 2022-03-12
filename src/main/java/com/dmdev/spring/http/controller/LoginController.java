2978e3db5664fe3884b2b8f2da38290201a5e6d5
dmatveyenka <dmdev2020@gmail.com>
2022-03-12 11:17:34 +0100

    lesson 81

--- src/main/java/com/dmdev/spring/http/controller/LoginController.java
+++ src/main/java/com/dmdev/spring/http/controller/LoginController.java
@@ -17,6 +17,8 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(Model model, @ModelAttribute("login") LoginDto loginDto) {
-        return "user/login";
+//        return "forward:/WEB-INF/jsp/user/login.jsp";
+//        return "redirect:https://google.com";
+        return "redirect:/login";
     }
 }
