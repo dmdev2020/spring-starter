ec5657946dbace7ce6d5996661f3d678937bea6a
dmatveyenka <dmdev2020@gmail.com>
2022-03-27 11:50:47 +0200

    lesson 93

--- src/main/java/com/dmdev/spring/http/controller/UserController.java
+++ src/main/java/com/dmdev/spring/http/controller/UserController.java
@@ -10,6 +10,7 @@ import com.dmdev.spring.service.UserService;
 import com.dmdev.spring.validation.group.CreateAction;
 import com.dmdev.spring.validation.group.UpdateAction;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -27,6 +28,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.groups.Default;
 
+@Slf4j
 @Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
---
+++ src/main/java/com/dmdev/spring/http/handler/ControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.dmdev.spring.http.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
+
+    @ExceptionHandler(Exception.class)
+    public String handleExceptions(Exception exception, HttpServletRequest request) {
+        log.error("Failed to return response", exception);
+        return "error/error500";
+    }
+}
