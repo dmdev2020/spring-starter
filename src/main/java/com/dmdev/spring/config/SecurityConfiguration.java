754f19dc54fb45635423499e65458b1048504e00
dmatveyenka <dmdev2020@gmail.com>
2022-04-11 09:45:12 +0200

    lesson 102

---
+++ src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.dmdev.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        .permitAll());
+    }
+}
--- src/main/java/com/dmdev/spring/http/controller/LoginController.java
+++ src/main/java/com/dmdev/spring/http/controller/LoginController.java
@@ -1,11 +1,7 @@
 package com.dmdev.spring.http.controller;
 
-import com.dmdev.spring.dto.LoginDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -13,12 +9,5 @@ public class LoginController {
     @GetMapping("/login")
     public String loginPage() {
         return "user/login";
-    }
-
-    @PostMapping("/login")
-    public String login(Model model, @ModelAttribute("login") LoginDto loginDto) {
-//        return "forward:/WEB-INF/jsp/user/login.jsp";
-//        return "redirect:https://google.com";
-        return "redirect:/login";
     }
 }
