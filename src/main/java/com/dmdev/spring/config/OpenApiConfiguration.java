1887e9e66c97a90363f148a7e5b4392b71419726
dmatveyenka <dmdev2020@gmail.com>
2022-04-24 10:32:31 +0200

    lesson 115

---
+++ src/main/java/com/dmdev/spring/config/OpenApiConfiguration.java
@@ -0,0 +1,21 @@
+package com.dmdev.spring.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "oauth2",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "http://localhost:8080/oauth2/authorization/google",
+                        tokenUrl = "https://www.googleapis.com/oauth2/v4/token"
+                )
+        )
+)
+public class OpenApiConfiguration {
+}
