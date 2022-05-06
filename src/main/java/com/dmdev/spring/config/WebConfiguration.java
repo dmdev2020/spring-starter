ff3aa8e717058b46ff8917e8dace074cc6fa127f
dmatveyenka <dmdev2020@gmail.com>
2022-05-06 15:51:38 +0300

    lesson 117

--- src/main/java/com/dmdev/spring/config/WebConfiguration.java
+++ src/main/java/com/dmdev/spring/config/WebConfiguration.java
@@ -1,14 +1,40 @@
 package com.dmdev.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
     public void addFormatters(FormatterRegistry registry) {
 //        registry.addConverter(Jsr310Converters.StringToLocalDateConverter.INSTANCE);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
     }
 }
