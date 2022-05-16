173c9b7273bd87a065bc0c2686a13633b0958d7e
dmatveyenka <dmdev2020@gmail.com>
2022-05-16 09:44:46 +0200

    lesson 119

---
+++ src/main/java/com/dmdev/spring/aop/FirstAspect.java
@@ -0,0 +1,80 @@
+package com.dmdev.spring.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FirstAspect {
+
+    /*
+        @within - check annotation on the class level
+     */
+    @Pointcut("@within(org.springframework.stereotype.Controller)")
+    public void isControllerLayer() {
+    }
+
+    /*
+        within - check class type name
+     */
+    @Pointcut("within(com.dmdev.spring.service.*Service)")
+    public void isServiceLayer() {
+    }
+
+    /*
+        this - check AOP proxy class type
+        target - check target object class type
+     */
+    @Pointcut("this(org.springframework.data.repository.Repository)")
+//    @Pointcut("target(org.springframework.data.repository.Repository)")
+    public void isRepositoryLayer() {
+    }
+
+    /*
+        @annotation - check annotation on method level
+     */
+    @Pointcut("isControllerLayer() && @annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void hasGetMapping() {
+    }
+
+    /*
+        args - check method param type
+        * - any param type
+        .. - 0+ any params type
+     */
+    @Pointcut("isControllerLayer() && args(org.springframework.ui.Model,..)")
+    public void hasModelParam() {
+    }
+
+    /*
+        @args - check annotation on the param type
+        * - any param type
+        .. - 0+ any params type
+     */
+    @Pointcut("isControllerLayer() && @args(com.dmdev.spring.validation.UserInfo,..)")
+    public void hasUserInfoParamAnnotation() {
+    }
+
+    /*
+        bean - check bean name
+     */
+    @Pointcut("bean(*Service)")
+    public void isServiceLayerBean() {
+    }
+
+    /*
+        execution(modifiers-pattern? ret-type-pattern declaring-type-pattern?name-pattern(param-pattern) throws-pattern?)
+     */
+    @Pointcut("execution(public * com.dmdev.spring.service.*Service.findById(*))")
+    public void anyFindByIdServiceMethod() {
+    }
+
+
+
+
+
+
+
+
+}
