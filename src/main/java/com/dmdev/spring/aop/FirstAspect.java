cf165e82b3c2512c44d979cee1742dceff9ab7bc
dmatveyenka <dmdev2020@gmail.com>
2022-05-18 09:45:17 +0200

    lesson 121

--- src/main/java/com/dmdev/spring/aop/FirstAspect.java
+++ src/main/java/com/dmdev/spring/aop/FirstAspect.java
@@ -1,10 +1,12 @@
 package com.dmdev.spring.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Aspect
@@ -73,10 +75,19 @@ public class FirstAspect {
     public void anyFindByIdServiceMethod() {
     }
 
-    @Before("anyFindByIdServiceMethod()")
+    @Before(value = "anyFindByIdServiceMethod() " +
+            "&& args(id) " +
+            "&& target(service) " +
+            "&& this(serviceProxy)" +
+            "&& @within(transactional)",
+            argNames = "joinPoint,id,service,serviceProxy,transactional")
 //    @Before("execution(public * com.dmdev.spring.service.*Service.findById(*))")
-    public void addLogging() {
-        log.info("invoked findById method");
+    public void addLogging(JoinPoint joinPoint,
+                           Object id,
+                           Object service,
+                           Object serviceProxy,
+                           Transactional transactional) {
+        log.info("invoked findById method in class {}, with id {}", service, id);
     }
 
 
