f3f510c9eb423fe502b5d82e1bf4b307869ee2f4
dmatveyenka <dmdev2020@gmail.com>
2022-05-21 15:25:05 +0200

    lesson 122

--- src/main/java/com/dmdev/spring/aop/FirstAspect.java
+++ src/main/java/com/dmdev/spring/aop/FirstAspect.java
@@ -2,6 +2,9 @@ package com.dmdev.spring.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -87,7 +90,26 @@ public class FirstAspect {
                            Object service,
                            Object serviceProxy,
                            Transactional transactional) {
-        log.info("invoked findById method in class {}, with id {}", service, id);
+        log.info("before - invoked findById method in class {}, with id {}", service, id);
+    }
+
+    @AfterReturning(value = "anyFindByIdServiceMethod() " +
+            "&& target(service)",
+            returning = "result")
+    public void addLoggingAfterReturning(Object result, Object service) {
+        log.info("after returning - invoked findById method in class {}, result {}", service, result);
+    }
+
+    @AfterThrowing(value = "anyFindByIdServiceMethod() " +
+            "&& target(service)",
+            throwing = "ex")
+    public void addLoggingAfterThrowing(Throwable ex, Object service) {
+        log.info("after throwing - invoked findById method in class {}, exception {}: {}", service, ex.getClass(), ex.getMessage());
+    }
+
+    @After("anyFindByIdServiceMethod() && target(service)")
+    public void addLoggingAfterFinally(Object service) {
+        log.info("after (finally) - invoked findById method in class {}", service);
     }
 
 
